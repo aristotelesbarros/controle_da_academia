f5e1223ee3e5e05c33675150240cc6e4882c47db
Aristoteles Barros Dal Farra <143902784+aristotelesbarros@users.noreply.github.com>
2024-05-11 17:24:50 -0300

    11/05/2024

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 
 const fs = require("fs")
+const data = require ("./data.json")
 
 exports.post = function(req,res){
         const keys = Object.keys (req.body)
@@ -8,8 +9,19 @@ exports.post = function(req,res){
                 return res.send ("formulario invalido")
             }
         }
+        req.body.created_at = Date.now()
+        req.body.birth = Date.parse(req.body.birth)
+        req.body.id = Number (data.instructors.length)+1
 
-     fs.writeFile("data.json",JSON.stringify(req.body),function(err){
+       let {avatar_url,gender,services,created_at,id} = req.body
+       
+       birth = Date.parse(birth)
+       created_at = Date.now()
+       id = Number (data.instructors.length)
+
+
+
+     fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
        if (err) return res.send("erro ao salvar o arquivo")
      return res.redirect ("/instructors")
     
